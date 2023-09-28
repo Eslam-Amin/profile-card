c23e5e8d473d59b6660d296c1649893377b2bb30
Eslam Amin <Eslam.amin.610@gmail.com>
2023-09-28 04:21:06 +0300

    changing in index.html

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
 import './App.css';
+import { React } from 'react';
+
+
+const skills = [{
+  name: "HTML+CSS",
+  emoji: "💪🏼",
+  backgroundColor: "blue",
+}, {
+  name: "javaScript",
+  emoji: "💪🏼",
+  backgroundColor: "orange",
+
+},
+{
+  name: "React",
+  emoji: "👶🏽",
+  backgroundColor: "lightgreen",
+
+},
+{
+  name: "API",
+  emoji: "💪🏼",
+  backgroundColor: "red",
+
+},
+{
+  name: "Web Design",
+  emoji: "👍🏼",
+  backgroundColor: "lightblue",
+
+},
+{
+  name: "Git & GitHub",
+  emoji: "👶🏽",
+  backgroundColor: "orangered",
+
+}
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='card'>
+      <Avatar />
+      <div className='data'>
+        <Intro />
+        {
+
+        }
+        <SkillList />
+      </div>
+
     </div>
   );
+}
+
+function Avatar() {
+  return (
+    <div>
+      <img className='avatar' src={require('./myAvatarImage.jpg')} alt="formal Avatar" ></img>
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <div className='data'>
+      <h1>Eslam Amin</h1>
+      <p>Junior Backend Developer and working to become
+        a FullStack developer by working on my FrontEnd Skills with React,
+        I am self-motivated learner, Hardworking developer,
+        I See an opportunity and I take it.
+        I don't compete with others, I only compete with
+        my yesterday-self.
+      </p>
+    </div>
+  )
+}
+
+function SkillList() {
+  return (
+    <div className='skill-list'>
+      {
+        skills.map((skill) => (
+          <Skill skill={skill} key={skill.name} />
+        ))
+      }
+    </div>
+  )
+}
+function Skill(props) {
+  const skill = props.skill;
+  return (<div className='skill' style={{ backgroundColor: skill.backgroundColor }}>
+    <span>{skill.name}</span>
+    <span>{skill.emoji}</span>
+  </div>)
 }
 
 export default App;
